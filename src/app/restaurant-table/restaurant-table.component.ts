6dd69720d1d2f82203b60df94db875e0fe606364
lennartmac <lennartpopma@hotmail.com>
2024-08-30 18:12:42 +0200

    Area filtering added...

--- src/app/restaurant-table/restaurant-table.component.ts
+++ src/app/restaurant-table/restaurant-table.component.ts
@@ -7,10 +7,19 @@ import { HttpClient } from '@angular/common/http';
   styleUrls: ['./restaurant-table.component.css']
 })
 export class RestaurantTableComponent implements OnInit {
-
   searchQuery: string = '';
-  sortColumn: string = '';
-  sortDirection: string = 'asc'; // 'asc' for ascending, 'desc' for descending
+  sortColumn: string = 'rating';
+  sortDirection: string = 'desc'; // 'asc' for ascending, 'desc' for descending
+
+  filters = {
+    all: true,
+    center: false,
+    north: false,
+    west: false,
+    south: false,
+    east: false,
+    southeast: false
+  };
 
   restaurants: any[] = [];
 
@@ -26,7 +35,8 @@ export class RestaurantTableComponent implements OnInit {
     let filtered = this.restaurants.filter(restaurant => {
       const nameMatches = restaurant.name.toLowerCase().includes(this.searchQuery.toLowerCase());
       const typeMatches = restaurant.type ? restaurant.type.toLowerCase().includes(this.searchQuery.toLowerCase()) : false;
-      return nameMatches || typeMatches;
+      const areaMatches = this.applyFilters(restaurant.area);
+      return (nameMatches || typeMatches) && areaMatches;
     });
 
     if (this.sortColumn) {
@@ -60,12 +70,47 @@ export class RestaurantTableComponent implements OnInit {
     return filtered;
   }
 
+  applyFilters(area: string | null): boolean {
+    if (this.filters.all) {
+      return true;
+    }
+
+    if (!area) {
+      return false;
+    }
+
+    const lowerCaseArea = area.toLowerCase();
+    return (this.filters.center && lowerCaseArea.includes('centrum')) ||
+           (this.filters.north && lowerCaseArea.includes('noord')) ||
+           (this.filters.west && lowerCaseArea.includes('west')) ||
+           (this.filters.south && (lowerCaseArea.includes('zuid') && !lowerCaseArea.includes('zuidoost'))) ||
+           (this.filters.east && (lowerCaseArea.includes('oost') && !lowerCaseArea.includes('zuidoost'))) ||
+           (this.filters.southeast && lowerCaseArea.includes('zuidoost'));
+  }
+
   sort(column: string) {
     if (this.sortColumn === column) {
       this.sortDirection = this.sortDirection === 'asc' ? 'desc' : 'asc';
     } else {
       this.sortColumn = column;
       this.sortDirection = 'asc';
+    }
+  }
+
+  toggleFilter(area: string, checked: boolean) {
+    if (area === 'all') {
+      this.filters = {
+        all: true,
+        center: false,
+        north: false,
+        west: false,
+        south: false,
+        east: false,
+        southeast: false
+      };
+    } else {
+      this.filters.all = false;
+      this.filters[area] = checked;
     }
   }
 }
